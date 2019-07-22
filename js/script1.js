fc698bf261e568785fba90b4bcee2e7c9c692b61
Bchizi <leonbichanga@gmail.com>
2019-07-22 10:27:30 +0300

    add fonts and made a function that will alert a messaeg

--- js/script1.js
+++ js/script1.js
@@ -1,13 +1,27 @@
 $(document).ready(function () {
     $('#p1,#p2,#p3').hide();
     $('#img_toggle1').click(function () {
-        $('#p1').toggle();
+        $('#p1').toggleclass();
     });
     $('#img_toggle2').click(function () {
-        $('#p2').toggle();
+        $('#p2').toggleclass();
     });
     $('#img_toggle3').click(function () {
-        $('#p3').toggle();
+        $('#p3').toggleclass();
     });
 
+});
+
+$(document).ready(function () {
+    $('#submit').click(function () {
+        var Name = $('#Name').val();
+        var Email = $('#Email').val();
+        var Message = $('#Message');
+        if (Name == '' || Email == '' || Message == '') {
+            alert('Please make sure you have filled in the form correctly!');
+        } else {
+            alert('We have received your message. Thank you for reaching out to us.');
+        }
+
+    })
 });
